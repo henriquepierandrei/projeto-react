5008a85d333a275d3f6c95ff2da66c8b5718fe04
Henrique Pierandrei <150195684+henriquepierandrei1@users.noreply.github.com>
2024-09-04 20:36:17 -0300

    commit inicial

---
+++ App.jsx
@@ -0,0 +1,21 @@
+
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
