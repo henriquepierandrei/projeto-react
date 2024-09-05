74bd15e8b896350f34920d1c8ac62150c7055a66
Henrique Pierandrei <henriquepierandrei1@gmail.com>
2024-09-05 20:28:58 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
 export default defineConfig({
-  plugins: [react()], // Certifique-se de que está correto
+  plugins: [react()],
   base: "/projeto-react/",
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
 });
